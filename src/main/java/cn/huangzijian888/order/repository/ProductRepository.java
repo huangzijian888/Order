f47e260cb876589f4b77399a599cec8b0c3d41e5
huangzijian888 <huangzijian888@gmail.com>
2019-10-02 02:55:51 +0800

    开启分类、商品接口跨域访问

--- src/main/java/cn/huangzijian888/order/repository/ProductRepository.java
+++ src/main/java/cn/huangzijian888/order/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package cn.huangzijian888.order.repository;
 
 import cn.huangzijian888.order.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author huangzijian888
  */
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
